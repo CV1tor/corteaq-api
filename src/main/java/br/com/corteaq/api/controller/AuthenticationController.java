5b2106f3c2e076f2a4fd33fdfa484e95673eecbc
Carlos <cvitor.dev@gmail.com>
2024-12-19 15:40:45 -0300

    Feature: configuração do CORS

--- src/main/java/br/com/corteaq/api/controller/AuthenticationController.java
+++ src/main/java/br/com/corteaq/api/controller/AuthenticationController.java
@@ -11,13 +11,11 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class AuthenticationController {
     private final TokenService tokenService;
     private final AuthenticationManager authenticationManager;
